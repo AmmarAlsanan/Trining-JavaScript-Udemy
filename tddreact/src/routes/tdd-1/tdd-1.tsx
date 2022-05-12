39f101b76e4c51a66047e9e0f9df933134adf3f6
amahfouz <amahfouz.c@tawal.com.sa>
2022-05-12 17:00:57 +0300

    tdd-react-1: check the first testing

---
+++ tddreact/src/routes/tdd-1/tdd-1.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const tdd1Component:React.FC =()=>{
+    return (
+        <React.Fragment><p>first video udmy course </p></React.Fragment>
+    )
+}
+
+export const Tdd1 =React.memo(tdd1Component)
