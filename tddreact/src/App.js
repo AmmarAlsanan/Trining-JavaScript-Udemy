b6a3240397cffbda101102b4e2681aaa8b741c62
amahfouz <amahfouz.c@tawal.com.sa>
2022-08-27 13:47:34 +0300

    reactLearn:01- create the app

--- tddreact/src/App.js
+++ tddreact/src/App.js
@@ -1,6 +1,31 @@
 import './App.css';
 
 function App() {
+
+  function increment(input){return input +1;}
+function decrement(input){return input -1;}
+function halve (input){return input /2;}
+
+const API_URL ='https://hub.dummyapis.com/employee?noofRecords=10&idStarts=1001';
+
+const pipline=[
+    increment,increment,decrement,increment,halve,increment
+];
+const initial_value= 1;
+
+const final_result =pipline.reduce((acc,fn)=> fn(acc),initial_value);
+
+const result_end =pipline.reduceRight((acc,fun)=>fun(acc),initial_value);
+
+console.log(final_result,result_end);
+
+
+const respnsePromise = fetch(API_URL +'films');
+
+respnsePromise.then(response=>{
+    return  console.log(response.length)
+})
+
   return (
     <div className="App">
       <header className="App-header">
--- tddreact/src/ui/components/section2.tsx
+++ tddreact/src/ui/components/section2.tsx
@@ -2,6 +2,8 @@ import { Button, Typography } from 'antd'
 import React from 'react'
 
  const Counter:React.FC=()=>{
+
+  
     const [counter,setCounter]=React.useState<number>(0);
     const {Text}=Typography;
     return (<React.Fragment>
