c8cc53c04e017fd975d3c347cbb8d2f9ca0691bf
amahfouz <amahfouz.c@tawal.com.sa>
2022-08-28 09:32:44 +0300

    reactLearn:01- cc

--- learningreact/src/App.js
+++ learningreact/src/App.js
@@ -1,8 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import { AddNumber } from './ui/components/addNumber';
+import { AddNumber } from './ui/components';
+import axios from 'axios'
+
+
 
 function App() {
+
+  //async /await 
+      const fetchingUser = async (size) => {
+        
+        const respone = fetch('https://dummyjson.com/products/1')
+  
+          .then(res => res.json())
+          
+          .then(json => console.log(json))
+        
+
+            
+        return respone
+      }
+
+
+  
+  //then / catch 
+  fetchingUser(1).catch(title => console.log(title)).catch(err => err.message)
+  
+  console.log(fetchingUser(2))
+
 
   return (
     <div className="App">
@@ -12,7 +37,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
           
         </p>
-      <AddNumber/>
+        <AddNumber />
       </header>
 
     </div>
