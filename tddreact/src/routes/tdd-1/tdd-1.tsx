2fadd85f56aba71a80216cbc5f629535a85ce2e4
amahfouz <amahfouz.c@tawal.com.sa>
2022-10-05 11:50:01 +0300

    FL-1:create react app for Login Form

--- tddreact/src/routes/tdd-1/tdd-1.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const tdd1Component:React.FC =()=>{
-    return (
-        <React.Fragment><p>first video udmy course </p></React.Fragment>
-    )
-}
-
-export const Tdd1 =React.memo(tdd1Component)
