354275f9eed793ba77540d899f4269b612acb25f
amahfouz <amahfouz.c@tawal.com.sa>
2022-10-13 15:09:28 +0300

    lerna & set packages

---
+++ DS/packages/ReadMe.ts
@@ -0,0 +1,14 @@
+
+/*=============================================
+=       Mange the multiple packages          =
+=============================================*/
+
+
+/* we will use Lerna 
+Lerna allows us to manage multiple packages inside the same repository instead of creating one repository per package.
+*/
+
+
+
+/*=====  End of Mange the multiple packages  ======*/
+
