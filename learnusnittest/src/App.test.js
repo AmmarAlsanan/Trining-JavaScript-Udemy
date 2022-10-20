0a4119cd07fe34d16f0e3aee41b926ea327a0e89
amahfouz <amahfouz.c@tawal.com.sa>
2022-10-20 14:16:29 +0300

    add jest and enzyme

--- learnusnittest/src/App.test.js
+++ learnusnittest/src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { configure ,EnzymeAdapter} from 'enzyme';
 
+configure({ adapter: new EnzymeAdapter() });
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+
